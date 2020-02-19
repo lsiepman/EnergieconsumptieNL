9d7b5d986192d540e3e4389a171a6e129ea76d7c
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2020-02-19 18:00:35 +0100

    finished correcting static map code after code review

--- Code/example_map_static.py
+++ Code/example_map_static.py
@@ -1,33 +1,34 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan 14 11:57:08 2020
+Created on Tue Jan 14 11:57:08 2020.
 
 @author: laura
 """
-#%% IMPORTS
-import functions_general as fct
-import functions_static_map as fsm
+
+import os
 import pandas as pd
 import cartopy.io.shapereader as shpreader
-import os
+from functions_static_map import StaticMap
+
+def main():
+    """Execute file"""
+    # SETTINGS
+    os.chdir("..")
+    pd.set_option('max_columns', 25)
+
+    DATA_PATH = os.getcwd() + "/Data/"
+    RESULT_PATH = os.getcwd() + "/Results/National_Map_Static/"
+
+    # DATA
+    shape_loc = DATA_PATH + "gadm36_NLD_shp/gadm36_NLD_1.shp"
+    shapes = list(shpreader.Reader(shape_loc).geometries())
+    data = pd.read_csv(DATA_PATH + "data_energy_geo.csv")
+
+    # PLOT
+    gas_2008 = StaticMap(data, 2008, "gas")
+    gas_2008.plotStaticMap("Gas usage in the Netherlands (2008)",
+                           "Gas usage (m3)", shapes, RESULT_PATH)
 
 
-#%% SETTINGS
-os.chdir("../Data")
-pd.set_option('max_columns', 25)
-
-#%% DATA
-shapes = list(shpreader.Reader("gadm36_NLD_shp/gadm36_NLD_1.shp").geometries())
-data = pd.read_csv("data_energy_geo.csv")
-
-#%% DATA PREP
-data = data.loc[(data["year"] == 2008) & (data["type"] == "gas")]
-data = fct.CorrectForConnection(data)
-data = fct.RemoveOutliers(data, "annual_consume_corrected")
-data = data.dropna(subset = ["LAT", "LON"])
-
-#%% PLOT
-os.chdir("../Results/National_Map_Static")
-
-fsm.PlotStaticMap(data, "annual_consume_corrected", "Gas usage in the Netherlands (2008)", "Gas usage (m3)", shapes)
-
+if __name__ == "__main__":
+    main()
--- Code/functions_static_map.py
+++ Code/functions_static_map.py
@@ -7,53 +7,94 @@ Created on Thu Feb 13 14:03:56 2020
 
 #%% IMPORTS
 import cartopy.crs as ccrs
-import branca.colormap as bcm
 import matplotlib.pyplot as plt
 from matplotlib import colors
 from matplotlib import cm as cmx
+from functions_general import GeneralFunctions
 
 #%%
+class StaticMap:
+    """Functions for plotting static maps"""
 
-def PlotStaticMap(data, col_usage, title, label_colorbar, shapes, extent = [3, 8, 50.5, 54]):
-    """
-    Plots a static map of energy usage in the Netherlands. The colour of the points indicates the usage.
-    
-    Parameters
-    ------------
-    data : pandas dataframe
-        contains information about energy usage on specific locations. The longitude and latitude data should be in columns called 'LON' and 'LAT'.
-    col_usage : str
-        name of the column containing the usage data
-    title : str
-        title of the plot
-    label_colorbar : str
-        title of the color bar
-    shapes : polygons
-        a (list of) polygons that are used to draw the map
-    extent : list of 4 int
-        determines the zoom level and focus of the map. Has a default value that shows the entire country.     
-    
-    Returns
-    --------
-    Plot in the working directory
-    
-    """
-    min_data = min(data[col_usage])
-    max_data = max(data[col_usage])
-    
-    fig = plt.figure(figsize=(8, 5))
-    ax = plt.axes(projection=ccrs.PlateCarree())
-    ax.add_geometries(shapes, ccrs.PlateCarree(),
-                  edgecolor='black', facecolor='gray', alpha=0.2)
-    ax.set_extent(extent, ccrs.PlateCarree())
-    norm = colors.Normalize(vmin = min_data, vmax = max_data)
-    cmap = plt.get_cmap('RdYlGn_r') 
-    m = cmx.ScalarMappable(cmap=cmap, norm = norm)
+    def __init__(self, data, year, energy_type):
+        """
+        Select data for static map
 
-    for point in range(len(data[col_usage])):
-        plt.plot(data["LON"].iloc[point], data["LAT"].iloc[point], 'o', color = m.to_rgba(data[col_usage].iloc[point]),transform=ccrs.PlateCarree())
-    plt.title(title)
-    cb = plt.colorbar(m)
-    cb.set_label(label_colorbar)
-    
-    plt.savefig(title, dpi = 300)
+        Parameters
+        ----------
+        data : pandas dataframe
+            Dataframe with energy consumption.
+        year : int
+            Year to plot.
+        energy_type : str
+            Which energy type to plot. "electricity" or "gas"
+
+        Returns
+        -------
+        None.
+
+        """
+
+        self.energy_type = energy_type
+        self.year = year
+        self.data_col = "annual_consume_corrected"
+
+        condition1 = (data["year"] == self.year)
+        condition2 = (data["type"] == self.energy_type)
+        data = data.loc[condition1 & condition2]
+
+        data = data.dropna(subset = ["LAT", "LON"]).reset_index(drop=True)
+        data = GeneralFunctions.correctForConnection(data)
+        data = GeneralFunctions.removeOutliers(data, self.data_col)
+
+        self.data = data
+
+    def plotStaticMap(self, title, label_colorbar, shapes, path,
+                      extent=[3, 8, 50.5, 54]):
+        """
+        Plots a static map of energy usage in the Netherlands.
+
+        The colour of the points indicates the usage.
+
+        Parameters
+        ----------
+        title : str
+            title of the plot
+        label_colorbar : str
+            title of the color bar
+        shapes : polygons
+            a (list of) polygons that are used to draw the map
+        path : str
+            save location of the plot
+        extent : list of 4 int
+            determines the zoom level and focus of the map.
+            Has a default value that shows the entirety of the Netherlands.
+
+        Returns
+        --------
+        Plot in the working directory or a subfolder based on title
+
+        """
+        min_data = min(self.data[self.data_col])
+        max_data = max(self.data[self.data_col])
+
+        fig = plt.figure(figsize=(8, 5))
+        ax = plt.axes(projection=ccrs.PlateCarree())
+        ax.add_geometries(shapes, ccrs.PlateCarree(),
+                      edgecolor='black', facecolor='gray', alpha=0.2)
+        ax.set_extent(extent, ccrs.PlateCarree())
+        norm = colors.Normalize(vmin=min_data, vmax=max_data)
+        cmap = plt.get_cmap('RdYlGn_r')
+        m = cmx.ScalarMappable(cmap=cmap, norm=norm)
+
+        for point in range(len(self.data[self.data_col])):
+            plt.plot(self.data["LON"].iloc[point],
+                     self.data["LAT"].iloc[point],
+                     'o',
+                     color=m.to_rgba(self.data[self.data_col].iloc[point]),
+                     transform=ccrs.PlateCarree())
+        plt.title(title)
+        cb = plt.colorbar(m)
+        cb.set_label(label_colorbar)
+
+        plt.savefig(path + title, dpi=300)
