87b3d6d9a01089fdff82b632ec0d55582c9c7951
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2020-02-19 14:13:30 +0100

    fixed general functions file to adhere to conventions

--- Code/functions_general.py
+++ Code/functions_general.py
@@ -19,16 +19,27 @@ class GeneralFunctions:
         """Correct energy usage for the number of active connections.
 
         Parameters
-        -----------
+        ----------
         data : pandas dataframe
-            contains the columns "annual_consume", "perc_of_active_connections", and "num_connections"
+            contains the columns "annual_consume",
+            "perc_of_active_connections", and
+            "num_connections"
 
         Returns
-        ---------
+        -------
         original dataframe with a new column for the corrected_annual_consume
         """
-        data = data.loc[data["perc_of_active_connections"]!= 0]
-        data["annual_consume_corrected"] = data["annual_consume"]/(data["num_connections"]*(data["perc_of_active_connections"]/100))
+        data = data.loc[data["perc_of_active_connections"] != 0]
+
+        orig_vals = data["annual_consume"]
+        connects = data["num_connections"]
+        perc_connects = data["perc_of_active_connections"]/100
+
+        cor_connects = connects*perc_connects
+
+        cor_vals = orig_vals/cor_connects
+
+        data["annual_consume_corrected"] = cor_vals
 
         return data
 
@@ -37,37 +48,29 @@ class GeneralFunctions:
         """Remove outliers from the data.
 
         Parameters
-        ------------
+        ----------
         data : pandas dataframe
             the data containing possible outliers
         name_col : str
             name of the column to be analysed
 
         Returns
-        ------------
+        -------
         dataframe without the outliers
         """
-
-        data.reset_index(inplace = True, drop = True)
+        data.reset_index(inplace=True, drop=True)
         threshold = 3
         mean_col = data[name_col].mean()
         std_col = data[name_col].std()
-        data["outlier"] = False
+        data["not_outlier"] = True
 
         for i in range(len(data)):
             value = data[name_col][i]
             z_score = (value - mean_col)/std_col
             if np.abs(z_score) > threshold:
-                data["outlier"][i] = True
+                data["not_outlier"][i] = False
 
-        data = data.loc[data["outlier"] == False]
-        data = data.drop(["outlier"], axis = 1)
+        data = data.loc[data["not_outlier"]]
+        data = data.drop(["not_outlier"], axis=1)
 
         return data
-
-
-
-
-
-
-
